b21264e6d126184b050c59c87c3a706f23f736a8
mapineda Pineda <mapineda1222@gmail.com>
2015-09-08 23:09:00 -0500

    created image variables and added css for the canvas

--- game.js
+++ game.js
@@ -1,4 +1,23 @@
+// define canvas var
+var c, canvas;
 // insert window onload function
 window.onload = function() {
+	canvas = document.getElementById('canvas');
+	c = canvas.getContext('2d')//method of every document of canvas element
 
+	draw();
 }
+//create image variables
+var bg = new Image();
+var l = new Image();
+var o = new Image();
+bg.src = './board.png';
+l.src = './one.png';
+o.src = './zero.png';
+
+function draw () {
+	c.clearRect(0,0,500,500);
+
+	c.
+
+};
